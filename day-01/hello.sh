af477597f70a4f173323fb921f4415cebe9ab600
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-10-25 20:22:11 -0400

    completed day 1 script

---
+++ day-01/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a script for Cloud Security Insight
+
+echo "Cloud Security Insights: Hello Derek"
+
+echo "Participants: Welcome to the world of DevOps, congratulations"
+
+echo "Cloud Security Insights: Scripting is the best skill for DevOps engineers"
