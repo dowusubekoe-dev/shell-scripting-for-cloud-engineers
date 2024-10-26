67fe5f8d8f2134b6c64d1b0449992151907d5c2d
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-10-26 06:17:59 -0400

    create variables

---
+++ day-02/my-portfolio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This is the portfolio for Derek Owusu Bekoe
+
+<< comment
+Anything in this multi-line comment tags will not be executed
+comment
+
+
+# Create a variable
+
+name="Derek"
+
+echo "Name is $name: "
+
+echo "Name is $name, and today's date is $(date)"
+
+echo "Enter the name: "
+
+read username
+
+echo "You entered $username"
+
+echo "The characters in $0 are: $1 $2"
